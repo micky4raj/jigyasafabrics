bff19ab6b1c1411436c6a70c078c6f46f0fc2ad1
Micky <micky4raj@gmail.com>
2026-09-01 02:47:48 +0530

    Fix syntax error in postgres.ts

--- src/lib/postgres.ts
+++ src/lib/postgres.ts
@@ -1,5 +1,7 @@
 import { Pool } from 'pg';
 
-export const pgPool = new Pool{{
-  connectionString: process.env.POSTGRES_URI || 'postgres://postgres:postgres@localhost:5432/jigyasa_db',
-}};
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+export default pool;
